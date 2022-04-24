387f2adc006bd479891db491a41695b0d9c6d371
Micael Dego <micaelalujandego@hotmail.com>
2022-04-24 18:15:45 -0300

    Ultimas modificaciones

--- Appblog/models.py
+++ Appblog/models.py
@@ -4,15 +4,13 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.views.generic import ListView, DeleteView, UpdateView, DetailView
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
-
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Post (models.Model):
     titulo = models.CharField(max_length=1000, primary_key= True)
     subtitulo = models.CharField(max_length=1000)
     introduccion = models.CharField(max_length=4000)
-    cuerpo = models.CharField(max_length=2000)
+    cuerpo = RichTextField()
     autor = models.CharField(max_length=40)
     fecha  = models.DateField()
     imagen = models.ImageField(upload_to='avatares', null= True, blank = True)
--- proyectofinal/settings.py
+++ proyectofinal/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Appblog',
     'Appperfil',
-    'Appmensaje'
+    'Appmensaje',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
